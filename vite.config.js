5340d20e4721ce07d4281b404002ebfd2a8b922e
TswelopeleMojewa-SQL-DEV <tswelopele@payday.co.za>
2025-10-30 10:29:53 +0200

    Added spme stuff in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     chunkSizeWarningLimit: 1000,
   },
+  server: {
+    historyApiFallback: true
+  }
 }
 
 )
